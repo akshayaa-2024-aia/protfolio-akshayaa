d71772d21da75f379ea6937bbd84e56a95a47b6b
akshayaa-2024-aia <akshayaasudhaharan13@gmail.com>
2024-10-27 17:35:28 +0530

    Add files via upload

---
+++ protfolio.py
@@ -0,0 +1,32 @@
+import streamlit as st
+st.title("Akshayaa's - Portfolio")
+
+st.header("About Me")
+st.write("""
+I'm Akshayaa, a 1st-year B.Tech student in Artificial Intelligence and Data Science.
+""")
+
+st.header("Interests")
+st.write("""
+* Artificial Intelligence
+* Data Science
+* Web Development
+""")
+
+st.header("Education")
+st.write("""
+* Ist year B.Tech in Artificial Intelligence and Data Science, 
+  College Name:KGisl institue of technology 
+  Academic Year:2024-2028
+""")
+
+st.header("Contact")
+st.write("""
+* Email: akshayaasudhaharan13@gmail.com
+""")
+
+st.header("Goals")
+st.write("""
+* Learn AI concepts
+* Build personal projects and contribute to open-source projects
+""")
